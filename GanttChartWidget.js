cd50c038d40a28489543cc1c7e0a29449a03bf2e
planifyit.com <contact@planifyit.com>
2023-07-14 22:04:16 +0200

    Update GanttChartWidget.js

--- GanttChartWidget.js
+++ GanttChartWidget.js
@@ -26,18 +26,14 @@
             momentScript.onload = () => this._momentReady = true;
             this._shadowRoot.appendChild(momentScript);
 
-            // Load SystemJS
-            const systemJsScript = document.createElement('script');
-            systemJsScript.src = 'https://unpkg.com/systemjs/dist/system.js';
-            systemJsScript.onload = () => {
-                // Load frappe-gantt using SystemJS
-                SystemJS.import('https://unpkg.com/frappe-gantt').then((module) => {
-                    this.Gantt = module.default;
-                    this._frappeGanttReady = true;
-                    this._renderChart();
-                });
+            // Load frappe-gantt ES5 version
+            const frappeGanttScript = document.createElement('script');
+            frappeGanttScript.src = 'https://unpkg.com/frappe-gantt/dist/frappe-gantt.js';
+            frappeGanttScript.onload = () => {
+                this._frappeGanttReady = true;
+                this._renderChart();
             };
-            this._shadowRoot.appendChild(systemJsScript);
+            this._shadowRoot.appendChild(frappeGanttScript);
         }
 
         // GanttChart methods
@@ -91,7 +87,7 @@
         _renderChart() {
             if (this._frappeGanttReady) {
                 const chartElement = this._shadowRoot.getElementById('chart');
-                new this.Gantt(chartElement, this.tasks, {
+                new Gantt(chartElement, this.tasks, {
                     view_mode: 'Month',
                     language: 'en'
                 });
