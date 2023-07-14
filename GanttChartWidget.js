7ba0526d5a4631fa10dcd2a55864e086e51d9427
planifyit.com <contact@planifyit.com>
2023-07-14 14:38:05 +0200

    Update GanttChartWidget.js

--- GanttChartWidget.js
+++ GanttChartWidget.js
@@ -1,4 +1,10 @@
 (function() {
+    const DAY_WIDTH = 10;
+    const HEADER_HEIGHT = 50;
+    const DEFAULT_ROW_PADDING = 10;
+    const DEFAULT_WIDTH = 500;
+    const SCALE_FACTOR = 2;
+
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
     <style>
@@ -34,53 +40,54 @@
     `;
 
     class GanttChartWidget extends HTMLElement {
-constructor() {
-    super();
-    this._shadowRoot = this.attachShadow({mode: 'open'});
-    this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-    this._props = {};
-  // Initialize GanttChart properties
-        this.milestones = new Map();
-        this.canvas = null;
-        this.canvasWidth = null;
-        this.canvasHeight = null;
-        this.ctx = null;
-    
-    // Load moment.js
-    const momentScript = document.createElement('script');
-    momentScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js';
-    momentScript.onload = () => this._momentReady = true;
-    this._shadowRoot.appendChild(momentScript);
+        constructor() {
+            super();
+            this._shadowRoot = this.attachShadow({mode: 'open'});
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._props = {};
+            // Initialize GanttChart properties
+            this.milestones = new Map();
+            this.canvas = null;
+            this.canvasWidth = null;
+            this.canvasHeight = null;
+            this.ctx = null;
+        
+            // Load moment.js
+            const momentScript = document.createElement('script');
+            momentScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js';
+            momentScript.onload = () => this._momentReady = true;
+            this._shadowRoot.appendChild(momentScript);
 
-    // Load date-fns
-    const dateFnsScript = document.createElement('script');
-    dateFnsScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/date-fns/2.27.0/date-fns.min.js';
-    dateFnsScript.onload = () => this._dateFnsReady = true;
-    this._shadowRoot.appendChild(dateFnsScript);
-}
-
+            // Load date-fns
+            const dateFnsScript = document.createElement('script');
+            dateFnsScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/date-fns/2.27.0/date-fns.min.js';
+            dateFnsScript.onload = () => this._dateFnsReady = true;
+            this._shadowRoot.appendChild(dateFnsScript);
+        }
 
         // GanttChart methods
-    initializeCanvas(parentElt) {
-        this.canvas = document.createElement("canvas");
-        parentElt.appendChild(this.canvas);
+        initializeCanvas(parentElt) {
+            this.canvas = document.createElement("canvas");
+            parentElt.appendChild(this.canvas);
 
-        this.canvasWidth = this.canvas.outerWidth || DEFAULT_WIDTH;
-        this.canvasHeight =
-            this.canvas.outerHeight ||
-            HEADER_HEIGHT + (this.milestones.size * (DEFAULT_ROW_HEIGHT + (DEFAULT_ROW_PADDING * 2)));
+            this.canvasWidth = this.canvas.outerWidth || DEFAULT_WIDTH;
+            this.canvasHeight =
+                this.canvas.outerHeight ||
+                HEADER_HEIGHT + (this.milestones.size * (DEFAULT_ROW_HEIGHT + (DEFAULT_ROW_PADDING * 2)));
 
-        this.canvas.style.width = `${this.canvasWidth / SCALE_FACTOR}px`;
-        this.canvas.style.height = `${this.canvasHeight / SCALE_FACTOR}px`;
+            this.canvas.style.width = `${this.canvasWidth / SCALE_FACTOR}px`;
+            this.canvas.style.height = `${this.canvasHeight / SCALE_FACTOR}px`;
 
-        this.canvas.width = this.canvasWidth;
-        this.canvas.height = this.canvasHeight;
+            this.canvas.width = this.canvasWidth;
+            this.canvas.height = this.canvasHeight;
 
-        this.ctx = this.canvas.getContext("2d");
-    }
-        
+            this.ctx = this.canvas.getContext("2d");
+        }
+            
         onCustomWidgetBeforeUpdate(changedProperties) {
             this._props = { ...this._props, ...changedProperties };
+            this._props.fontSize = changedProperties.fontSize;
+            this._props.elementHeight = changedProperties.elementHeight;
         }
 
         onCustomWidgetAfterUpdate(changedProperties) {
@@ -89,79 +96,79 @@ constructor() {
             }
         }
 
-_updateData(dataBinding) {
-    console.log('dataBinding:', dataBinding);
-    if (!dataBinding) {
-        console.error('dataBinding is undefined');
-    }
-    if (!dataBinding || !dataBinding.data) {
-        console.error('dataBinding.data is undefined');
-    }
-    
-    if (this._momentReady && this._dateFnsReady) {
-        // Check if dataBinding and dataBinding.data are defined
-        if (dataBinding && Array.isArray(dataBinding.data)) {
-            // Transform the data into the correct format
-            const transformedData = dataBinding.data.map(row => {
-                console.log('row:', row);
-                // Check if dimensions_0, dimensions_1, measures_0, and measures_1 are defined before trying to access their properties
-                if (row.dimensions_0 && row.dimensions_1 && row.measures_0 && row.measures_1) {
-                    return {
-                        id: row.dimensions_0.label,
-                        label: row.dimensions_1.label,
-                        startDate: moment(row.measures_0.raw).format('YYYY-MM-DD'),
-                        endDate: moment(row.measures_1.raw).format('YYYY-MM-DD'),
-                        dependsOn: []  // You would need to modify this if your data includes dependencies
-                    };
+        _updateData(dataBinding) {
+            console.log('dataBinding:', dataBinding);
+            if (!dataBinding) {
+                console.error('dataBinding is undefined');
+            }
+            if (!dataBinding || !dataBinding.data) {
+                console.error('dataBinding.data is undefined');
+            }
+            
+            if (this._momentReady && this._dateFnsReady) {
+                // Check if dataBinding and dataBinding.data are defined
+                if (dataBinding && Array.isArray(dataBinding.data)) {
+                    // Transform the data into the correct format
+                    const transformedData = dataBinding.data.map(row => {
+                        console.log('row:', row);
+                        // Check if dimensions_0, dimensions_1, measures_0, and measures_1 are defined before trying to access their properties
+                        if (row.dimensions_0 && row.dimensions_1 && row.measures_0 && row.measures_1) {
+                            return {
+                                id: row.dimensions_0.label,
+                                label: row.dimensions_1.label,
+                                startDate: moment(row.measures_0.raw).format('YYYY-MM-DD'),
+                                endDate: moment(row.measures_1.raw).format('YYYY-MM-DD'),
+                                dependsOn: []  // You would need to modify this if your data includes dependencies
+                            };
+                        }
+                    }).filter(Boolean);  // Filter out any undefined values
+
+                    // Store the milestones
+                    this.milestones = new Map(transformedData.map(milestone => [milestone.id, milestone]));
+
+                    // Calculate the start date of the Gantt chart
+                    this.startDate = Array.from(this.milestones.values()).reduce((min, milestone) => {
+                        const startDate = new Date(milestone.startDate);
+                        return startDate < min ? startDate : min;
+                    }, new Date(Infinity));
+
+                    // Render the chart
+                    this._renderChart();
+                } else {
+                    console.error('Data is not an array:', dataBinding && dataBinding.data);
                 }
-            }).filter(Boolean);  // Filter out any undefined values
+            }
+        }
 
-            // Store the milestones
-            this.milestones = new Map(transformedData.map(milestone => [milestone.id, milestone]));
+        _renderChart() {
+            // Clear the chart element
+            const chartElement = this._shadowRoot.getElementById('chart');
+            while (chartElement.firstChild) {
+                chartElement.removeChild(chartElement.firstChild);
+            }
 
-            // Calculate the start date of the Gantt chart
-            this.startDate = Array.from(this.milestones.values()).reduce((min, milestone) => {
-                const startDate = new Date(milestone.startDate);
-                return startDate < min ? startDate : min;
-            }, new Date(Infinity));
+            // Initialize the canvas
+            this.initializeCanvas(chartElement);
 
-            // Render the chart
-            this._renderChart();
-        } else {
-            console.error('Data is not an array:', dataBinding && dataBinding.data);
+            // Set the font size
+            this.ctx.font = `${this._props.fontSize || 12}px sans-serif`;
+
+            // Draw a rectangle for each milestone
+            let y = HEADER_HEIGHT;
+            for (let milestone of this.milestones.values()) {
+                // Calculate the x coordinates of the start and end of the rectangle
+                const startX = dateFns.differenceInDays(new Date(milestone.startDate), this.startDate) * DAY_WIDTH;
+                const endX = dateFns.differenceInDays(new Date(milestone.endDate), this.startDate) * DAY_WIDTH;
+
+                // Draw the rectangle
+                this.ctx.fillStyle = 'blue';
+                this.ctx.fillRect(startX, y, endX - startX, this._props.elementHeight || 20);
+
+                // Move to the next row
+                y += (this._props.elementHeight || 20) + (DEFAULT_ROW_PADDING * 2);
+            }
         }
     }
-}
-
-
-_renderChart() {
-    // Clear the chart element
-    const chartElement = this._shadowRoot.getElementById('chart');
-    while (chartElement.firstChild) {
-        chartElement.removeChild(chartElement.firstChild);
-    }
-
-    // Initialize the canvas
-    this.initializeCanvas(chartElement);
-
-    // Set the font size
-    this.ctx.font = `${this._props.fontSize || 12}px sans-serif`;
-
-    // Draw a rectangle for each milestone
-    let y = HEADER_HEIGHT;
-    for (let milestone of this.milestones.values()) {
-        // Calculate the x coordinates of the start and end of the rectangle
-        const startX = dateFns.differenceInDays(new Date(milestone.startDate), this.startDate) * DAY_WIDTH;
-        const endX = dateFns.differenceInDays(new Date(milestone.endDate), this.startDate) * DAY_WIDTH;
-
-        // Draw the rectangle
-        this.ctx.fillRect(startX, y, endX - startX, this._props.elementHeight || 20);
-
-        // Move to the next row
-        y += (this._props.elementHeight || 20) + (DEFAULT_ROW_PADDING * 2);
-    }
-}
-
 
     customElements.define('gantt-chart-widget', GanttChartWidget);
 })();
