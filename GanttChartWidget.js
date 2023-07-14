cb7ac355c2ebc5eba4d2288dba6d77be8f645789
planifyit.com <contact@planifyit.com>
2023-07-14 22:45:36 +0200

    Update GanttChartWidget.js

--- GanttChartWidget.js
+++ GanttChartWidget.js
@@ -17,6 +17,7 @@
     class GanttChartWidget extends HTMLElement {
         constructor() {
             super();
+            console.log('Constructor called');
             this._shadowRoot = this.attachShadow({mode: 'open'});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._props = {};
@@ -40,6 +41,7 @@
 
         // GanttChart methods
         static get metadata() {
+            console.log('metadata called');
             return {
                 properties: {
                     myDataBinding: {
@@ -51,10 +53,12 @@
         }
 
         onCustomWidgetBeforeUpdate(changedProperties) {
+            console.log('onCustomWidgetBeforeUpdate called');
             this._props = { ...this._props, ...changedProperties };
         }
 
         onCustomWidgetAfterUpdate(changedProperties) {
+            console.log('onCustomWidgetAfterUpdate called');
             if ("myDataBinding" in changedProperties) {
                 const dataBinding = changedProperties.myDataBinding;
                 if (dataBinding.state === 'success') {
@@ -63,50 +67,52 @@
             }
         }
 
-      _updateData(dataBinding) {
-    if (this._momentReady) {
-        if (dataBinding && Array.isArray(dataBinding.data)) {
-            this.tasks = dataBinding.data.map(row => {
-                if (row.dimensions_0 && row.dimensions_1 && row.dimensions_2 && row.dimensions_3) {
-                    const startDate = new Date(row.dimensions_2.id);
-                    const endDate = new Date(row.dimensions_3.id);
-                    // Check if startDate and endDate are valid dates
-                    if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) {
-                        console.error('Invalid date:', row.dimensions_2.id, row.dimensions_3.id);
-                        return null;
-                    }
-                    // Check if startDate is before endDate
-                    if (startDate > endDate) {
-                        console.error('Start date is after end date:', startDate, endDate);
-                        return null;
-                    }
-                    console.log('startDate:', startDate, 'endDate:', endDate);  // Log the start and end dates
-                    return {
-                        id: row.dimensions_0.label,
-                        name: row.dimensions_1.label,
-                        start: startDate,
-                        end: endDate,
-                        progress: 0,
-                        dependencies: ''
-                    };
-                }
-            }).filter(Boolean);  // Filter out any null values
+        _updateData(dataBinding) {
+            console.log('_updateData called');
+            if (this._momentReady) {
+                if (dataBinding && Array.isArray(dataBinding.data)) {
+                    this.tasks = dataBinding.data.map(row => {
+                        if (row.dimensions_0 && row.dimensions_1 && row.dimensions_2 && row.dimensions_3) {
+                            const startDate = new Date(row.dimensions_2.id);
+                            const endDate = new Date(row.dimensions_3.id);
+                            // Check if startDate and endDate are valid dates
+                            if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) {
+                                console.error('Invalid date:', row.dimensions_2.id, row.dimensions_3.id);
+                                return null;
+                            }
+                            // Check if startDate is before endDate
+                            if (startDate > endDate) {
+                                console.error('Start date is after end date:', startDate, endDate);
+                                return null;
+                            }
+                            console.log('startDate:', startDate, 'endDate:', endDate);  // Log the start and end dates
+                            return {
+                                id: row.dimensions_0.label,
+                                name: row.dimensions_1.label,
+                                start: startDate,
+                                end: endDate,
+                                progress: 0,
+                                dependencies: ''
+                            };
+                        }
+                    }).filter(Boolean);  // Filter out any null values
 
-            // Check if all tasks have valid start and end dates
-            for (let task of this.tasks) {
-                if (task.start === null || task.end === null) {
-                    console.error('Task with null start or end date:', task);
+                    // Check if all tasks have valid start and end dates
+                    for (let task of this.tasks) {
+                        if (task.start === null || task.end === null) {
+                            console.error('Task with null start or end date:', task);
+                        }
+                    }
+
+                    console.log('Tasks:', this.tasks);  // Log the tasks
+
+                    this._renderChart();
                 }
             }
-
-            console.log('Tasks:', this.tasks);  // Log the tasks
-
-            this._renderChart();
         }
-    }
-}
 
         _renderChart() {
+            console.log('_renderChart called');
             if (this._frappeGanttReady) {
                 const chartElement = this._shadowRoot.getElementById('chart');
                 new Gantt(chartElement, this.tasks, {
