1a125f1f8879cd96408ef28d9ba5d137b4e43bd6
planifyit.com <contact@planifyit.com>
2023-07-17 16:31:49 +0200

    Update GanttChartWidget.js

--- GanttChartWidget.js
+++ GanttChartWidget.js
@@ -7,21 +7,12 @@
     border: 1px solid #000;
     padding: 10px;
     margin: 10px;
-  
+    width: 100%;  /* Set the width */
+    max-width: 95%; /* Add this line */
+    height: 500px;  /* Set the height */
+    overflow: auto;  /* Add this line */
+    box-sizing: border-box;  /* Add this line */
 }
-
-.fn-gantt .leftPanel .spacer {  
-height: 72px  !important;    }
-
-.fn-gantt .row.header {      
-overflow: hidden;   }
-.fn-gantt .dataPanel {
-        width: 216px !important;
-        height: 144px  !important;
-    }
-.fn-gantt .rightPanel .month, .fn-gantt .rightPanel .year { 
-width: 72px !important;
-   }
 
     </style>
     <div id="chart"></div>
@@ -38,19 +29,20 @@ constructor() {
     this._props = {};
     this.tasks = [];
 
-    // Load jQuery UI CSS
-      const jQueryUICSS = document.createElement('link');
-    jQueryUICSS.rel = 'stylesheet';
-    jQueryUICSS.href = 'https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css';
-    this._shadowRoot.appendChild(jQueryUICSS);
-
-    // Load jQuery Gantt CSS
-    const jQueryGanttCSS = document.createElement('link');
-    jQueryGanttCSS.rel = 'stylesheet';
-    jQueryGanttCSS.href = 'https://cdn.jsdelivr.net/gh/taitems/jQuery.Gantt@master/css/style.css';
-    this._shadowRoot.appendChild(jQueryGanttCSS);
 
 
+    // Load Frappe Gantt
+const FrappeGanttScript = document.createElement('script');
+FrappeGanttScript.src = 'https://unpkg.com/frappe-gantt';
+FrappeGanttScript.onload = () => {
+    // Frappe Gantt is now loaded and can be used.
+    this._FrappeGanttReady = true;
+    this._renderChart();
+};
+this._shadowRoot.appendChild(FrappeGanttScript);
+    
+
+    
     // Load jQuery
     const jQueryScript = document.createElement('script');
     jQueryScript.src = 'https://code.jquery.com/jquery-3.7.0.min.js';
@@ -111,68 +103,86 @@ constructor() {
                 }
             }
         }
+
 _updateData(dataBinding) {
     console.log('_updateData called');
     if (dataBinding && Array.isArray(dataBinding.data)) {
-        this.tasks = [
-            {
-                name: "Task 1",
-                desc: "Description 1",
-                values: [{
-                    from: "/Date(1640995200000)/",
-                    to: "/Date(1641081600000)/",
-                    label: "Task 1",
-                    customClass: "ganttRed"
-                }]
-            },
-            {
-                name: "Task 2",
-                desc: "Description 2",
-                values: [{
-                    from: "/Date(1641081600000)/",
-                    to: "/Date(1641168000000)/",
-                    label: "Task 2",
-                    customClass: "ganttRed"
-                }]
+        this.tasks = dataBinding.data.map((row, index) => {
+            if (row.dimensions_0 && row.dimensions_1 && row.dimensions_2 && row.dimensions_3) {
+                const startDate = new Date(row.dimensions_2.id);
+                const endDate = new Date(row.dimensions_3.id);
+                // Check if startDate and endDate are valid dates
+                if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) {
+                    console.error('Invalid date:', row.dimensions_2.id, row.dimensions_3.id);
+                    return null;
+                }
+                // Check if startDate is before endDate
+                if (startDate > endDate) {
+                    console.error('Start date is after end date:', startDate, endDate);
+                    return null;
+                }
+                console.log('startDate:', startDate, 'endDate:', endDate);  // Log the start and end dates
+                return {
+                    id: 'Task ' + (index + 1),  // Unique id of task
+                    name: row.dimensions_0.label,  // Name of task
+                    start: startDate.toISOString().split('T')[0],  // Start date of task
+                    end: endDate.toISOString().split('T')[0],  // End date of task
+                    progress: 0,  // Progress of task in percent
+                    dependencies: ''  // Dependencies of task
+                };
             }
-        ];
+        }).filter(Boolean);  // Filter out any null values
+
+        // Check if all tasks have valid start and end dates
+        for (let task of this.tasks) {
+            if (!task.start || !task.end) {
+                console.error('Task with null start or end date:', task);
+            }
+        }
+
+        console.log('Tasks:', this.tasks);  // Log the tasks
+
         this._renderChart();
     }
 }
 
-
+        
 _renderChart() {
     console.log('_renderChart called');
-    if (this._jQueryGanttReady) {
-        console.log('this._jQuery.fn:', this._jQuery.fn);  // Log this._jQuery.fn to the console
-        console.log('this._jQuery:', this._jQuery);  // Log this._jQuery to the console
-        console.log('jQuery.fn.gantt:', jQuery.fn.gantt);  // Log jQuery.fn.gantt to the console
-        const chartElement = this._shadowRoot.getElementById('chart');
+    const chartElement = this._shadowRoot.getElementById('chart');
 
-        // Use $.ready to ensure that jquery.fn.gantt.js is loaded
-        this._jQuery(document).ready(() => {
-            // Add a delay before creating the chart
-            setTimeout(() => {
-                jQuery(chartElement).gantt({  // Use jQuery instead of this._jQuery
-                    source: this.tasks,
-                    navigate: 'scroll',
-                    scale: 'days',
-                    maxScale: 'months',
-                    minScale: 'days',
-                    itemsPerPage: 5,
-                    onItemClick: function(data) {
-                        alert('Item clicked - show some details');
-                    },
-                    onAddClick: function(dt, rowId) {
-                        alert('Empty space clicked - add an item!');
-                    },
-                    onRender: function() {
-                        console.log('chart rendered');
-                    }
-                });
-            }, 100);  // delay of 100 milliseconds
+    // Load Frappe Gantt
+    const FrappeGanttScript = document.createElement('script');
+    FrappeGanttScript.src = 'https://unpkg.com/frappe-gantt@0.5.0/dist/frappe-gantt.min.js';
+    FrappeGanttScript.onload = () => {
+        // Create a new Gantt chart
+        const gantt = new Gantt(chartElement, this.tasks, {
+            on_click: function(task) {
+                console.log(task);
+            },
+            on_date_change: function(task, start, end) {
+                console.log(task, start, end);
+            },
+            on_progress_change: function(task, progress) {
+                console.log(task, progress);
+            },
+            on_view_change: function(mode) {
+                console.log(mode);
+            },
+            custom_popup_html: function(task) {
+                // the task object will contain the updated
+                // dates and progress value
+                return `
+                    <div class="details-container">
+                        <h5>${task.name}</h5>
+                        <p>Expected to finish by ${task.end}</p>
+                        <p>${task.progress}% completed!</p>
+                    </div>
+                `;
+            }
         });
-    }
+    };
+    this._shadowRoot.appendChild(FrappeGanttScript);
 }
 
 
