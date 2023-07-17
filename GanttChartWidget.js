b3c1f775734ff7b78c631c32f4555c16df6b1f44
planifyit.com <contact@planifyit.com>
2023-07-17 16:19:18 +0000

    Update GanttChartWidget.js

--- GanttChartWidget.js
+++ GanttChartWidget.js
@@ -123,37 +123,39 @@ _updateData(dataBinding) {
 
 _renderChart() {
     console.log('_renderChart called');
-    console.log('Tasks:', this.tasks);
     if (this._FrappeGanttReady) {
         const chartElement = this.shadowRoot.getElementById('chart');
 
-        // Create a new Gantt chart
-        const gantt = new window.Gantt(chartElement, this.tasks, {
-            on_click: function(task) {
-                console.log(task);
-            },
-            on_date_change: function(task, start, end) {
-                console.log(task, start, end);
-            },
-            on_progress_change: function(task, progress) {
-                console.log(task, progress);
-            },
-            on_view_change: function(mode) {
-                console.log(mode);
-            },
-            custom_popup_html: function(task) {
-                // the task object will contain the updated
-                // dates and progress value
-                return `
-                    <div class="details-container">
-                        <h5>${task.name}</h5>
-                        <p>Expected to finish by ${task.end}</p>
-                        <p>${task.progress}% completed!</p>
-                    </div>
-                `;
-            }
-        });
-        console.log('Gantt:', gantt);  // Log the gantt object to the console
+        // Use setTimeout to add a delay before creating the chart
+        setTimeout(() => {
+            // Create a new Gantt chart
+            const gantt = new window.Gantt(chartElement, this.tasks, {
+                on_click: function(task) {
+                    console.log(task);
+                },
+                on_date_change: function(task, start, end) {
+                    console.log(task, start, end);
+                },
+                on_progress_change: function(task, progress) {
+                    console.log(task, progress);
+                },
+                on_view_change: function(mode) {
+                    console.log(mode);
+                },
+                custom_popup_html: function(task) {
+                    // the task object will contain the updated
+                    // dates and progress value
+                    return `
+                        <div class="details-container">
+                            <h5>${task.name}</h5>
+                            <p>Expected to finish by ${task.end}</p>
+                            <p>${task.progress}% completed!</p>
+                        </div>
+                    `;
+                }
+            });
+            console.log('Gantt:', gantt);  // Log the gantt object to the console
+        }, 1000);  // delay of 1 second
     }
 }
 
